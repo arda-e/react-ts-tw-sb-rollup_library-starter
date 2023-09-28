2b61d55d5a65e2d2cfb90ceb215a4363cb1d9ba6
Meric Arda Eren <aeren@netas.com.tr>
2023-09-28 12:15:50 +0300

    Init rollup & typescript

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+          dir: 'dist',
+          format: 'esm',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript({
+                tsconfig: './tsconfig.json',
+                declaration: true,
+                declarationDir: 'dist',
+              }),
+        ]
+    }
+]
