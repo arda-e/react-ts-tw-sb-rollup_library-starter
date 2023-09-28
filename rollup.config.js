6ffa85ae80b41cb4263790956455b8f3ed12f3e5
Meric Arda Eren <aeren@netas.com.tr>
2023-09-28 15:39:04 +0300

    Fix build folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,46 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-//import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import peerDeps from 'rollup-plugin-peer-deps-external';
+import { glob } from 'glob'
 
-export default [
-    {
-        input: 'src/index.ts',
-        output: {
-          dir: 'dist',
-          format: 'esm',
-        //  preserveModules: true,
-        //  preserveModulesRoot: 'src',
-          sourcemap: true,
-        },
-        plugins: [
-            // peerDepsExternal(),
-            resolve(),
-            commonjs(),
-            typescript({
-                tsconfig: './tsconfig.json',
-                declaration: true,
-                declarationDir: 'dist',
-              }),
-        ],
-        // external: ['react'],
-    }
-]
+
+const excludeExtensions = [
+    'test.js',
+    'test.ts',
+    'test.jsx',
+    'test.tsx',
+    'stories.js',
+    'stories.ts',
+    'stories.jsx',
+    'stories.tsx',
+  ];
+  
+  const inputFiles = [
+    './src/index.ts',
+    ...glob.sync('./src/**/*.{ts,tsx}', {
+      ignore: excludeExtensions.map(ext => `**/*${ext}`),
+    }),
+  ];
+
+export default {
+    input: inputFiles,
+    output: {
+        dir: 'dist',
+        format: 'esm',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        sourcemap: true,
+    },
+    plugins:[
+        peerDeps(),
+        resolve(),
+        commonjs(),
+        typescript({
+          tsconfig: './tsconfig.build.json',
+          declaration: true,
+          declarationDir: 'dist',
+        }),
+    ],
+    external: ['react', 'react-dom'],
+}
